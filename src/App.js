886616bc34288e1cc1279e1640a671a5c4605a1f
VinayJangotra <109384165+VinayJangotra@users.noreply.github.com>
2024-01-15 17:35:57 +0530

    New Logo added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import About from './Components/About';
+import Tours from './Components/Tours';
+import Services from './Components/Services';
+import Footer from './Components/Footer';
+function App(){
+    return(
+    <>
+    <Navbar/>
+    <Hero/>
+    <About/>
+    <Services/>
+    <Tours/>
+    <Footer/>
+    </>
+    )
+};
 
 export default App;
